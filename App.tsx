6742bc47da57c9a81dc0a5617e04cc699cf573a9
Angela de Araujo <angela.araujo.rj@gmail.com>
2022-11-05 22:40:44 -0300

    Interface screen Pools

--- App.tsx
+++ App.tsx
@@ -1,7 +1,7 @@
 import { NativeBaseProvider, StatusBar } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_500Medium, Roboto_700Bold } from '@expo-google-fonts/roboto';
 import { AuthContextProvider } from './src/contexts/AuthContext';
-import { Find } from './src/screens/Find';
+import { Pools } from './src/screens/Pools';
 import { Loading } from './src/components/Loading';
 import { THEME } from './src/styles/theme';
 
@@ -16,7 +16,7 @@ export default function App() {
           backgroundColor={"transparent"}
           translucent
         />
-        {fontsLoaded ? <Find /> : <Loading />}
+        {fontsLoaded ? <Pools /> : <Loading />}
       </AuthContextProvider>
     </NativeBaseProvider>
   );
