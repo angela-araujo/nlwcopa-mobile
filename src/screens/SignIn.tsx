0be60038a79c85a7865b762833be9b42ba4f071a
Angela de Araujo <angela.araujo.rj@gmail.com>
2022-11-05 21:38:50 -0300

    Autenticação com Google

--- src/screens/SignIn.tsx
+++ src/screens/SignIn.tsx
@@ -10,8 +10,6 @@ export function SignIn() {
 
     const { signIn, user } = useAuth();
 
-    console.log('Dados do usuário => ', user);
-
     return (
         <Center flex={1} bgColor="gray.900" p={7}>
             <Logo width={212} height={40} />
