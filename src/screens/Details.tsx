6bbf8ddbc2f676cf1d8840dd5da1e759be383b73
Angela de Araujo <angela.araujo.rj@gmail.com>
2022-11-17 16:12:10 -0300

    implementação das opççoes palpites e ranking; busca de detalhes do bolão; compartilhamento de cõdigo do bolão

---
+++ src/screens/Details.tsx
@@ -0,0 +1,91 @@
+import { Share } from 'react-native';
+import { useEffect, useState } from 'react';
+import { HStack, useToast, VStack } from 'native-base';
+import { useRoute } from '@react-navigation/native';
+
+import { api } from '../services/api';
+
+import { Header } from '../components/Header';
+import { Option } from '../components/Option';
+import { Loading } from '../components/Loading';
+import { PollHeader } from '../components/PollHeader';
+import { PollCardProps } from '../components/PollCard';
+import { EmptyMyPollist } from '../components/EmptyMyPollList';
+
+interface RouteParams {
+    id: string;
+}
+
+export function Details() {
+    const [optionSelected, setOptionSelected] = useState<'guesses' | 'ranking'>('guesses');
+    const [isLoading, setIsLoading] = useState(true);
+    const [pollDetails, setPollDetais] = useState<PollCardProps>({} as PollCardProps);
+
+    const route = useRoute();
+    const toast = useToast();
+
+    const { id } = route.params as RouteParams;
+
+
+    async function fetchPollDetails() {
+        try {
+            setIsLoading(true);
+
+            const response = await api.get(`/polls/${id}`);
+            setPollDetais(response.data.poll);
+
+        } catch (error) {
+            console.log(error);
+
+            toast.show({
+                title: 'Não foi possível carregar os detalhes do bolão',
+                placement: 'top',
+                bgColor: 'red.500'
+            })
+        } finally {
+            setIsLoading(false);
+        }
+
+    }
+
+    async function handleCodeShare() {
+        await Share.share({
+            message: pollDetails.code
+        });
+    }
+
+    useEffect(() => {
+        fetchPollDetails();
+    }, [id]);
+
+    if (isLoading) {
+        return (
+            <Loading />
+        )
+    }
+
+    return (
+        <VStack flex={1} bgColor='gray.900'>
+            <Header title={pollDetails.title} showBackButton showShareButton onShare={handleCodeShare} />
+            {
+                pollDetails._count?.participants > 0 ?
+                    <VStack px={5} flex={1}>
+                        <PollHeader data={pollDetails} />
+                        <HStack bgColor={"gray.800"} p={1} rounded="sm" mb={5}>
+                            <Option
+                                title="Seus palpites"
+                                isSelected={optionSelected === 'guesses'}
+                                onPress={() => setOptionSelected('guesses')}
+                            />
+                            <Option
+                                title="Ranking do grpo"
+                                isSelected={optionSelected === 'ranking'}
+                                onPress={() => setOptionSelected('ranking')}
+                            />
+                        </HStack>
+                    </VStack> :
+                    <EmptyMyPollist code={pollDetails.code} />
+            }
+        </VStack>
+    );
+}
